692285fee00f4cb900f66d58ca2111752c792a38
𝓜𝓸𝓱𝓲𝓫 <126652872+mohib41@users.noreply.github.com>
2025-06-18 00:10:31 +0530

    Delete portfolio-generator directory

--- portfolio-generator/script.js
+++
@@ -1,5 +0,0 @@
-function generatePortfolio() {
-  const input = document.getElementById("markdown-input").value;
-  const htmlContent = marked.parse(input);
-  document.getElementById("preview").innerHTML = htmlContent;
-}
